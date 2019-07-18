d20958343a855105ba229684551f557dad2a542f
Will Brown <williambrown@williams-air.lan>
2019-07-18 00:08:56 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var shoutouts = [
+  "You are really funny and you are so helpful.(Tall friend)",
+  "Your humor and how you always make everyone feel included",
+  "I love your passion for shrek :)",
+  "Your unique personality. You bring a new perspective to every conversation. Go Shrek!",
+  "How you went with me to get sparkling water lol. And how you're obsessed with Shrek, and you're super funny and make me laugh. One of my favorite people here.",
+  "how you freely express your love for Shrek lol",
+  "You are really funny and great help! You are very talented at coding.",
+  "your endless enthusiasm and your ability to make anything fun, like getting water!",
+  "How funny you are and your taste in memes!",
+  "Your witty sense of humor and intelligence.",
+  "Your personality and the great convos you start!",
+  "how open you are to helping everybody and being so nice and funny you are. You made me so happy and comftorable",
+  "Your enthusiasm! You are just so full of energy and positivity, it is truly inspiring and contagious."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
